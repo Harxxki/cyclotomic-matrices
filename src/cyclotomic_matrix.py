d863128f30677b1d79d343b30a89d538b6853abd
Harxxki <haruki4dev@gmail.com>
2023-05-30 16:54:09 +0900

    Add type hint.

--- src/cyclotomic_matrix.py
+++ src/cyclotomic_matrix.py
@@ -1,22 +1,23 @@
 import sys
 from collections import namedtuple
+from typing import Union
 
 import numpy as np
 
 
-def power(base, exponent):
+def power(base: int, exponent: int) -> int:
     return base ** exponent
 
 
 class CyclotomicMatrix:
-    def __init__(self, p, l, generator, k):
+    def __init__(self, p: int, l: int, generator: int, k: int):
         self.p = p
         self.l = l
         self.generator = generator
         self.k = k
         self.matrix = self._generate()
 
-    def _generate(self):
+    def _generate(self) -> np.ndarray:
         """
         Generates a cyclotomic matrix of order 2l^2 over the field Z_p.
         :param p: A prime number
@@ -42,13 +43,12 @@ class CyclotomicMatrix:
                     for t in range(self.k):
                         p1 = 2 * self.l ** 2 * s + arr[a][b].l
                         p2 = 2 * self.l ** 2 * t + arr[a][b].m
-                        if (power(self.generator, p1) + 1 - power(self.generator,
-                                                                  p2)) % self.p == 0:
+                        if (power(self.generator, p1) + 1 - power(self.generator, p2)) % self.p == 0:
                             count += 1
                 arr[a][b] = arr[a][b]._replace(n=count)
         return arr
 
-    def _convert_cyclotomic_matrix_to_int_matrix(self):
+    def _convert_cyclotomic_matrix_to_int_matrix(self) -> np.ndarray:
         if self.matrix is None:
             raise ValueError("Matrix has not been generated yet.")
 
@@ -61,7 +61,7 @@ class CyclotomicMatrix:
 
         return arr
 
-    def get(self, only_n=False):
+    def get(self, only_n=False) -> Union[np.ndarray, int]:
         if only_n:
             return self._convert_cyclotomic_matrix_to_int_matrix()
         return self.matrix
