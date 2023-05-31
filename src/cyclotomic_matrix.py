f0f5306a63a8792bb7f71018e8b80990b2b703b0
Harxxki <haruki4dev@gmail.com>
2023-05-31 14:57:06 +0900

    Separate calc func.

--- src/cyclotomic_matrix.py
+++ src/cyclotomic_matrix.py
@@ -11,42 +11,48 @@ def power(base: int, exponent: int) -> int:
 
 class CyclotomicMatrix:
     def __init__(self, p: int, l: int, generator: int, k: int):
-        self.p = p
-        self.l = l
-        self.generator = generator
-        self.k = k
-        self.matrix = self._generate()
-
-    def _generate(self) -> np.ndarray:
         """
-        Generates a cyclotomic matrix of order 2l^2 over the field Z_p.
         :param p: A prime number
         :param l: A positive integer
         :param generator: A primitive root of p
         :param k: A positive integer
-        :return: A 2l^2 x 2l^2 matrix with entries from the set {0, 1, ..., k-1}^2l^2
         """
-        order = 2 * self.l ** 2
-        size = order
+        self.p = p
+        self.l = l
+        self.generator = generator
+        self.k = k
+        self.order = 2 * self.l ** 2
+        self.size = self.order
+        self.matrix = self._generate()
+        self._calc()
 
+    def _generate(self) -> np.ndarray:
+        """
+        Generates a cyclotomic matrix of order 2l^2 over the field Z_p.
+        """
         Entry = namedtuple('Entry', 'l m n')
-        arr = np.empty((size, size), dtype=object)
+        arr = np.empty((self.size, self.size), dtype=object)
 
-        for a in range(size):
-            for b in range(size):
+        for a in range(self.size):
+            for b in range(self.size):
                 arr[a][b] = Entry(l=a, m=b, n=0)
 
-        for a in range(size):
-            for b in range(size):
+        return arr
+
+    def _calc(self) -> None:
+        """
+        Calculates the entries of the cyclotomic matrix.
+        """
+        for a in range(self.size):
+            for b in range(self.size):
                 count = 0
                 for s in range(self.k):
                     for t in range(self.k):
-                        p1 = 2 * self.l ** 2 * s + arr[a][b].l
-                        p2 = 2 * self.l ** 2 * t + arr[a][b].m
+                        p1 = 2 * self.l ** 2 * s + self.matrix[a][b].l
+                        p2 = 2 * self.l ** 2 * t + self.matrix[a][b].m
                         if (power(self.generator, p1) + 1 - power(self.generator, p2)) % self.p == 0:
                             count += 1
-                arr[a][b] = arr[a][b]._replace(n=count)
-        return arr
+                self.matrix[a][b] = self.matrix[a][b]._replace(n=count)
 
     def _convert_cyclotomic_matrix_to_int_matrix(self) -> np.ndarray:
         if self.matrix is None:
