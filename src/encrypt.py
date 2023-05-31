929a5e31fdfb9cdae1fd4ef1f8779ed400f6ea83
Harxxki <haruki4dev@gmail.com>
2023-05-31 14:57:34 +0900

    Refactor.

--- src/encrypt.py
+++ src/encrypt.py
@@ -2,6 +2,7 @@ import sys
 
 from src.cyclotomic_matrix import CyclotomicMatrix
 from src.matrix_converter import MatrixConverter
+from utils import print_matrix
 
 
 def main():
@@ -18,20 +19,20 @@ def main():
     # Convert the message string to a message matrix
     mc = MatrixConverter(l)
     message_matrix = mc.str_to_matrix(message_str)
-    print("Message Matrix:")
-    for row in message_matrix:
-        for entry in row:
-            print(f"{entry:2}", end=" ")
-        print()
+    print_matrix(message_matrix, "Message Matrix")
 
     # Generate the cyclotomic matrix
     cm = CyclotomicMatrix(p, l, generator, k)
     cyclotomic_matrix = cm.get(only_n=True)
-    print("Cyclotomic Matrix:")
-    for row in cyclotomic_matrix:
-        for entry in row:
-            print(f"{entry:2}", end=" ")
-        print()
+    print_matrix(cyclotomic_matrix, "Cyclotomic Matrix")
+
+    # Encrypt the message
+    encrypted_matrix = cyclotomic_matrix @ message_matrix
+    print_matrix(encrypted_matrix, "Encrypted Matrix")
+
+    # Convert the encrypted matrix to a string
+    encrypted_message = mc.matrix_to_str(encrypted_matrix)
+    print(f"Encrypted Message: {encrypted_message}")
 
 
 if __name__ == "__main__":
