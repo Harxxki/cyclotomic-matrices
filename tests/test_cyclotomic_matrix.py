c2b3f026f5d217463a143421cc96194fcafa3f33
Harxxki <haruki4dev@gmail.com>
2023-06-21 22:03:47 +0900

    Update test.

--- tests/test_cyclotomic_matrix.py
+++ tests/test_cyclotomic_matrix.py
@@ -3,11 +3,7 @@ from src.utils import print_matrix
 from src.cyclotomic_matrix import CyclotomicMatrix
 
 
-def test_cyclotomic_matrix():
-    """
-    Test the cyclotomic matrix.
-    :return:
-    """
+def test_calculate():
     cm = CyclotomicMatrix(p=17, l=2, generator=3, k=2)
     matrix = cm.get(matrix_format="calculated")
 
@@ -29,5 +25,31 @@ def test_cyclotomic_matrix():
     np.testing.assert_array_equal(matrix, expected_output)
 
 
+def test_mul():
+    # TODO: implement
+    pass
+
+
+def test_inv():
+    cm = CyclotomicMatrix(p=17, l=2, generator=3, k=2).mul(r_0=7)
+    inv_matrix = cm.inv()
+
+    # D*
+    expected_output = np.array([
+        [-1, 1, 1, -1, -1, 1, -1, 1],
+        [1, 0, 0, 1, 0, 0, 0, -1],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [-1, 1, 0, -1, 0, 1, -1, 1],
+        [-1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 1, 0, -1, 1, -1],
+        [-1, 0, 0, -1, 0, 1, 0, 1],
+        [1, -1, 0, 1, 1, -1, 1, -1]
+    ])
+
+    np.testing.assert_array_equal(inv_matrix, expected_output)
+
+
 if __name__ == "__main__":
-    test_cyclotomic_matrix()
+    test_calculate()
+    test_mul()
+    test_inv()
