23e64f9bbb7e984acea09c56905456d899bfb66c
Harxxki <haruki4dev@gmail.com>
2023-06-26 23:12:39 +0900

    add float to int cast

--- src/matrix_converter.py
+++ src/matrix_converter.py
@@ -9,11 +9,11 @@ class MatrixConverter:
         return ord(c)
 
     def int_to_char(self, i):
-        return chr(i)
+        return chr(int(i))
 
     def str_to_matrix(self, s):
-        matrix_size = 2 * self.l**2
-        if len(s) > matrix_size**2:
+        matrix_size = 2 * self.l ** 2
+        if len(s) > matrix_size ** 2:
             raise ValueError("The input string is too large for the specified matrix size.")
         matrix = np.zeros((matrix_size, matrix_size), dtype=int)
         for idx, char in enumerate(s):
